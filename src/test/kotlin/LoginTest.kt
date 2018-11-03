1d0072327e321a3bed6ead80d40464c087aa5871
Christoph Loy <loy.christoph@gmail.com>
2018-11-03 17:45:19 +0100

    Added optional tests that depend on provided logindata

--- src/test/kotlin/LoginTest.kt
+++ src/test/kotlin/LoginTest.kt
@@ -1,14 +1,20 @@
 import de.beatbrot.grips.GripsClient
 import de.beatbrot.grips.model.LoginData
 import de.beatbrot.grips.model.Realm
-import io.kotlintest.shouldThrow
-import io.kotlintest.shouldThrowAny
+import io.kotlintest.*
+import io.kotlintest.extensions.SpecLevelExtension
+import io.kotlintest.extensions.TagExtension
+import io.kotlintest.extensions.TestListener
 import io.kotlintest.specs.StringSpec
 import java.lang.IllegalArgumentException
 
 class LoginTest : StringSpec({
-    val fooBarLogin = LoginData(Realm.HS, "foo", "bar")
 
+    val realm = System.getenv("GRIPS_REALM")
+    val username = System.getenv("GRIPS_USER")
+    val password = System.getenv("GRIPS_PW")
+
+    val fooBarLogin = LoginData(Realm.HS, "foo", "bar")
 
     "Logging in with wrong credentials should fail" {
         val client = GripsClient(fooBarLogin)
@@ -23,4 +29,26 @@ class LoginTest : StringSpec({
             client.createModel()
         }
     }
+
+    "Logging in with the real username should succeed".config(enabled = hasLoginData()) {
+        println(System.getenv())
+        realm shouldNotBe null
+        username shouldNotBe null
+        password shouldNotBe null
+
+        val realObj = Realm.valueOf(realm.toUpperCase())
+        val loginData = LoginData(realObj, username, password)
+
+        val client = GripsClient(loginData)
+        client.login()
+    }
 })
+
+fun hasLoginData(): Boolean {
+    val env = System.getenv()
+    val realm = env["GRIPS_REALM"]
+    val user = env["GRIPS_USER"]
+    val pw = env["GRIPS_PW"]
+
+    return !(realm == null || user == null || pw == null)
+}
